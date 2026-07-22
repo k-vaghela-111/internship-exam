4aec346c3cca0cd3832a05460e2ad20025d38e4f
vaghela karmrajsinh <kvaghela.it@gmail.com>
2026-07-22 15:56:00 +0530

    feat: add logging for login requests and simplify password hashing middleware

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -56,6 +56,8 @@ const registerUser = asyncHandler(async (req, res) => {
 
 const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
+    console.log("Request Body:", req.body);
+    console.log("Entered Password:", password);
 
     if (!email || !password) {
         throw new ApiError(400, "Email and Password are required");
